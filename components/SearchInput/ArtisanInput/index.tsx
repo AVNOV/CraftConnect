7b68ca476315bd3f9dfedbf45b76529a83f9c1d1
Corentin Faget <faget.corentin@gmail.com>
2023-01-06 18:11:48 +0100

    front(home-page): Use react hook form to handle data in search input component

--- components/SearchInput/ArtisanInput/index.tsx
+++ components/SearchInput/ArtisanInput/index.tsx
@@ -1,12 +1,30 @@
+import { forwardRef } from "react";
+
 import Image from "next/image";
 
 import search_icon from "../../../assets/icons/search_icon.svg";
 
-export default function ArtisanInput() {
+type props = {
+  name: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  value: string;
+};
+
+const ArtisanInput = forwardRef(({ name, onChange, value }: props, ref) => {
   return (
     <div className="flex items-center justify-center bg-white px-2 py-3 border-r">
-      <Image src={search_icon} alt="" className="w-6 h-6 invert-25"/>
-      <input placeholder="Nom, spécialité..." type="text" className="bg-transparent pl-4 text-lg"/>
+      <Image src={search_icon} alt="" className="w-6 h-6 invert-25" />
+      <input
+        name={name}
+        onChange={onChange}
+        value={value}
+        required={true}
+        placeholder="Nom, spécialité..."
+        type="text"
+        className="bg-transparent pl-4 text-lg"
+      />
     </div>
   );
-}
+});
+
+export default ArtisanInput;
--- components/SearchInput/LocationInput/index.tsx
+++ components/SearchInput/LocationInput/index.tsx
@@ -1,52 +1,60 @@
+import { forwardRef, useState } from "react";
+import { UseFormSetValue } from "react-hook-form";
+
 import Image from "next/image";
 import Geocode from "react-geocode";
 
 import marker_icon from "../../../assets/icons/marker_icon.svg";
 import position_icon from "../../../assets/icons/position_icon.svg";
-import { useState } from "react";
 
-export default function LocationInput() {
-  const [location, setLocation] = useState<string>("");
+type props = {
+  setValue: UseFormSetValue<{ artisan: string; location: string }>;
+  name: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  value: string;
+};
 
-  const handlePosition = () => {
-    navigator.geolocation.getCurrentPosition(
-      (position: GeolocationPosition) => {
-        Geocode.fromLatLng(
-          position.coords.latitude.toString(),
-          position.coords.longitude.toString()
-        ).then(
-          (response) => {
-            const address = response.results[0].formatted_address;
-            setLocation(address);
-          },
-          (error: Error) => {
-            console.error(error);
-          }
-        );
-      }
+const LocationInput = forwardRef(
+  ({ name, onChange, value, setValue }: props, ref) => {
+    const handlePosition = () => {
+      navigator.geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          Geocode.fromLatLng(
+            position.coords.latitude.toString(),
+            position.coords.longitude.toString()
+          ).then(
+            (response) => {
+              const address = response.results[0].formatted_address;
+              setValue("location", address);
+            },
+            (error: Error) => {
+              console.error(error);
+            }
+          );
+        }
+      );
+    };
+
+    return (
+      <div className="flex items-center justify-center bg-white px-2 py-3 border-r">
+        <Image src={marker_icon} alt="" className="w-6 h-6" />
+        <input
+          name={name}
+          onChange={onChange}
+          value={value}
+          placeholder="Où ?"
+          type="text"
+          className="bg-transparent pl-4 text-lg"
+        />
+        <Image
+          onClick={handlePosition}
+          src={position_icon}
+          alt="current position button"
+          className="w-6 h-6 cursor-pointer active:scale-95 transition-transform"
+        />
+      </div>
     );
-  };
+  }
+);
 
-  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setLocation(event.currentTarget.value);
-  };
-
-  return (
-    <div className="flex items-center justify-center bg-white px-2 py-3 border-r">
-      <Image src={marker_icon} alt="" className="w-6 h-6" />
-      <input
-        onChange={handleChange}
-        value={location}
-        placeholder="Où ?"
-        type="text"
-        className="bg-transparent pl-4 text-lg"
-      />
-      <Image
-        onClick={handlePosition}
-        src={position_icon}
-        alt="current position button"
-        className="w-6 h-6 cursor-pointer active:scale-95 transition-transform"
-      />
-    </div>
-  );
-}
+export default LocationInput;
