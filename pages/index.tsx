04ae4a485bf102e7f35549c9deee7c7e72b183fc
Corentin Faget <faget.corentin@gmail.com>
2023-01-07 00:26:27 +0100

    front(home-page): Add text on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ import Head from "next/head";
 import Image from "next/image";
 
 import background_decoration from "../assets/images/background_decoration.svg";
+import artisans_illustration from "../assets/images/artisans_illustration.png";
+import users_illustration from "../assets/images/users_illustration.png";
+
 import SearchInput from "../components/SearchInput";
 
 export default function Home() {
@@ -13,18 +16,40 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="h-full w-full">
-        <Image
-          src={background_decoration}
-          alt=""
-          className="absolute left-0 top-0 w-full"
-        />
-        <div className="relative z-10">
-          <div className="mt-20 ml-32">
-            <h2 className="text-4xl text-white">
-              Trouvez un rendez-vous avec votre artisan
-            </h2>
-            <SearchInput />
+      <main className="flex flex-col items-center relative h-full w-full">
+        <Image src={background_decoration} alt="" className="w-full" />
+        <div className="absolute top-32 left-32">
+          <h2 className="text-4xl text-white">
+            Trouvez un rendez-vous avec votre artisan
+          </h2>
+          <SearchInput />
+        </div>
+        <div className="text-center w-9/12 mt-10">
+          <h1 className="text-3xl">
+            CraftConnect - Tous les artisans dont vous avez besoin
+          </h1>
+          <p className="text-lg mt-5">
+            CraftConnect permet aux particuliers de prendre rendez-vous en ligne
+            chez des artisans professionnels de leur région. L’application vous
+            met en relation avec le professionnel de votre choix afin réaliser
+            les travaux de votre maison, votre appartement ou même votre jardin
+            !
+          </p>
+        </div>
+        <div className="flex space-x-24 py-20">
+          <div className="flex flex-col justify-between w-44 h-full">
+            <Image src={artisans_illustration} alt="" />
+            <p className="text-center mt-2">
+              Plus de <span className="text-red">300</span> artisans à votre
+              service
+            </p>
+          </div>
+          <div className="flex flex-col justify-between w-48 h-full">
+            <Image src={users_illustration} alt="" />
+            <p className="text-center mt-2">
+              Plus de <span className="text-red">100 000</span> utilisateurs
+              satisfait
+            </p>
           </div>
         </div>
       </main>
