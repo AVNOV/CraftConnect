64d6de95c712ed77cc44723c41e2a01db487dff6
Corentin Faget <faget.corentin@gmail.com>
2023-01-13 18:03:09 +0100

    front(search-page): Adapt SearchInput component for search page

--- components/SearchInput/index.tsx
+++ components/SearchInput/index.tsx
@@ -4,7 +4,11 @@ import SearchButton from "./SearchButton";
 
 import { useForm, Controller } from "react-hook-form";
 
-export default function SearchInput() {
+type props = {
+  size?: string;
+};
+
+export default function SearchInput({ size }: props) {
   const { handleSubmit, setValue, control } = useForm({
     defaultValues: {
       artisan: "",
@@ -13,25 +17,25 @@ export default function SearchInput() {
   });
 
   const onSubmit = (data: any) => {
-    console.log(data)
+    console.log(data);
   };
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="flex rounded overflow-hidden w-fit mt-5"
+      className="flex rounded overflow-hidden w-fit mt-5 shadow-searchcard"
     >
       <Controller
         name="artisan"
         control={control}
-        render={({ field }) => <ArtisanInput {...field} />}
+        render={({ field }) => <ArtisanInput size={size} {...field} />}
       />
       <Controller
         name="location"
         control={control}
-        render={({ field }) => <LocationInput setValue={setValue} {...field} />}
+        render={({ field }) => <LocationInput size={size}  setValue={setValue} {...field} />}
       />
-      <SearchButton />
+      <SearchButton size={size}  />
     </form>
   );
 }
---
+++ pages/search/index.tsx
@@ -0,0 +1,9 @@
+import SearchInput from "../../components/SearchInput";
+
+export default function Search() {
+  return (
+    <div className="flex flex-col items-center w-full h-full bg-grey-light">
+      <SearchInput size="small" />
+    </div>
+  );
+}
