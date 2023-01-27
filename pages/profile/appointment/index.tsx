ba9cf358e3e187ba9381e23d8b30b0c748d9d0ef
edouardLaffont <edouard.laffont@live.com>
2023-01-27 15:18:45 +0100

    feat(profile-appointment): init page profile/appointment

---
+++ pages/profile/appointment/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function ProfileAppointment() {
+  return (
+    <>
+      <Head>
+        <title>CraftConnect</title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex h-full">
+        Rendez-vous de l'utilisateur
+      </main>
+    </>
+  );
+}
