2cf718ae233322a56e382ca9e4039110b93f3b13
Corentin Faget <faget.corentin@gmail.com>
2023-01-27 15:03:44 +0100

    front(booking): page validation

--- pages/booking/confirmation/index.tsx
+++ pages/booking/confirmation/index.tsx
@@ -5,22 +5,28 @@ import moment from "moment";
 export default function Confirmation() {
   const router = useRouter();
 
+  const handleClick = () => {
+    router.push({ pathname: "validation", query: router.query });
+  };
+
   return (
     <div className="flex items-center justify-center h-full w-full">
       <div className="flex flex-col rounded bg-white shadow-searchcard w-4/12 p-5">
         <div>
-          <h3>Horaires</h3>
-          <p className="text-grey text-xs">{moment(router.query.date).format("DD MMMM YYYY à HH:mm")}</p>
+          <h3 className="mb-1">Horaires</h3>
+          <p className="text-grey text-xs">
+            {moment(router.query.date).format("DD MMMM YYYY à HH:mm")}
+          </p>
         </div>
         <div className="border-grey border-y-0.5 py-3 my-3">
-          <h3>Motif</h3>
+          <h3 className="mb-1">Motif</h3>
           <p className="text-grey text-xs">
             {router.query.reason} <br />
             {router.query.description}
           </p>
         </div>
         <div>
-          <h3>Coordonnées</h3>
+          <h3 className="mb-1">Coordonnées</h3>
           <p className="text-grey text-xs">
             {router.query.firstname} {router.query.lastname} <br />
             {router.query.email} <br />
@@ -28,7 +34,7 @@ export default function Confirmation() {
             {router.query.city}
           </p>
         </div>
-        <Button children="Confirmer" className="self-end mt-5" />
+        <Button onClick={handleClick} children="Confirmer" className="self-end mt-5" />
       </div>
     </div>
   );
