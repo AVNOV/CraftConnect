8d349de0b0532d90e7ba5088a7ce46bfe09c5a7f
edouardLaffont <edouard.laffont@live.com>
2023-01-29 12:25:59 +0100

    fix(profile-button): minor text fix

--- components/Header/ProfileButton/DropdownList/index.tsx
+++ components/Header/ProfileButton/DropdownList/index.tsx
@@ -18,7 +18,7 @@ export default function DropdownList() {
   const listItems = useMemo(
     () => [
       {
-        text: "Profile",
+        text: "Profil",
         href: "/profile",
       },
       {
