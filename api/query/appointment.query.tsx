2e25360c5b5875b28050fc22e2816884de27d3f1
edouardLaffont <edouard.laffont@live.com>
2023-02-02 19:38:11 +0100

    feat(appointment): delete appointment in user profile

--- api/query/appointment.query.tsx
+++ api/query/appointment.query.tsx
@@ -10,3 +10,8 @@ export const getAppointments = async () => {
   const { data } = await API.get("/appointments");
   return data;
 };
+
+export const deleteAppointment = async (id: number) => {
+  const { data } = await API.delete(`/appointments/${id}`);
+  return data;
+};
